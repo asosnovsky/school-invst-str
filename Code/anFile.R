0317a0b2cbcbc5d7c0a4b72d04190d5a0ddac968
Ariel Sosnovsky <Ariel Sosnovsky>
2016-01-30 23:25:36 -0500

    clean DATA

--- Code/anFile.R
+++ Code/anFile.R
@@ -18,13 +18,36 @@ VARs = ((scard[,dic[dic[,2]==2,1]]))
   ( suppressWarnings(VARs <- apply(VARs,2,as.numeric)) )
 Prec = ((scard[,dic[dic[,2]==3,1]])) 
   ( suppressWarnings(Prec <- apply(Prec,2,as.numeric)) )
-#c("INSTNM","PREDDEG","CURROPER")
 
 sData <- cbind(Info[,!grepl("CONTROL|LOCALE|DISTANCEONLY|CITY",names(Info))],VARs)
 
 #Merge
   sData <- merge(sData,poor_students)
+  
+  #IPEDS
+    ipeds.fin <- read.csv("Contest-Data/ipeds/finance.csv",stringsAsFactors=FALSE)
+      ipeds.fin <- (mutate(ipeds.fin, Tot_Grant=F1E01+F1E02+F1E05+F1E06))
+      sData <- merge(sData,ipeds.fin[,c(1,length(ipeds.fin))])
+      
+    ipeds.msc <- read.csv("Contest-Data/ipeds/misc.csv",stringsAsFactors=FALSE)
+      ipeds.msc <- ipeds.msc[,grepl('NPT41|UNITID',names(ipeds.msc))]
+      suppressWarnings( ipeds.msc <- apply(ipeds.msc,2, as.numeric) )
+      ipeds.msc <- cbind(ipeds.msc[,1],apply(ipeds.msc[,2:length(names(ipeds.msc))],1,sum))
+      colnames(ipeds.msc) <- c("UNITID","Avg_Income_Low")
+      sData <- merge(sData,ipeds.msc);
+                     
+    ipeds.tot <- read.csv("Contest-Data/ipeds/total_students.csv",stringsAsFactors=FALSE)
+      names(ipeds.tot) <- c("UNITID","Tot_Students")
+      sData <- merge(sData,ipeds.tot)
 
+#Compute Rank
+  sData <- mutate(sData, GG_NO_re=(1-gt_25k_p6)-POOR_Prop)
+  
+#Ensure only needed vars are kept
+  sData <- sData[!is.na(sData$GG_NO_re),]
+  sData <- sData[!is.na(sData$Avg_Income_Low),]
+  sData <- sData[!is.na(sData$Tot_Grant),] 
+  sData <- sData[!is.na(sData$Tot_Students),] 
 #Filtering
   sData <- filter(sData,CURROPER>0 & RELAFFIL >0) #Omit Inactive unis
   sData <- sData[!apply(sData[,names(Info)[!grepl("CONTROL|LOCALE|DISTANCEONLY|CITY|UNITID|INSTNM|PREDDEG|CURROPER|RELAFFIL",names(Info))]]>0,1,any),];    
@@ -39,7 +62,7 @@ for(name in names(spD)) {
   spD[[name]] = spD[[name]][,!grepl("PREDDEG",colnames(spD[[name]]))]
   spD[[name]] = spD[[name]][,as.logical(omitMe(spD[[name]]) < 0.5)]
   spD[[name]] = spD[[name]][as.logical(omitMe(spD[[name]],1) == 0),]
-  write.csv(spD[[name]],paste0('Analyzed-Data/',name,'-cleaned-data.csv'),row.names=FALSE)
+  write.csv(arrange(spD[[name]],desc(GG_NO_re)),paste0('Analyzed-Data/',name,'-cleaned-data.csv'),row.names=FALSE)
 }
 
 write.csv(sData,'Analyzed-Data/pre-cleaned-data.csv',row.names=FALSE)
