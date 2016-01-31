2c0b5c8ba2935ec3e867e08d01bc90674ec38677
Ariel Sosnovsky <Ariel Sosnovsky>
2016-01-31 16:10:07 -0500

    top-20-schools

--- Code/post_matlab_an.R
+++ Code/post_matlab_an.R
@@ -4,13 +4,25 @@ require('dplyr')
 setwd("C:/Github/school-invst-str/Code")
 
 partData <- read.csv("Maximized-Data/partitioned.csv");
-colnames(partData) = c("ID", "A")
+colnames(partData) = c("ID", "n", "A", "r")
 partName <- read.csv("Matlab-Ready-Data/parted.name.csv");
 
-partData <- mutate(partData, Name = partName[ID,2] )
+partData <- mutate(partData, Name = partName[ID,2] )%>%
+            filter(n>0) %>%
+            arrange(desc(r))
+
+
+prezPD <- partData[,c("Name","n","A","r")]
+colnames(prezPD) <- c("Name","Number of Students","Total Amount Invested","Increase in Poverty Allevation (ROI)")
+
+
 
 fullData <- read.csv("Maximized-Data/full.csv");
-colnames(fullData) = c("ID", "A")
+colnames(fullData) = c("ID", "n", "A", "r")
 fullName <- read.csv("Matlab-Ready-Data/full.name.csv");
 
-fullData <- mutate(fullData, Name = fullName[ID,2] )
+fullData <- mutate(fullData, Name = fullName[ID,2] )%>%
+            filter(n>0) %>%
+            arrange(desc(r))
+
+
