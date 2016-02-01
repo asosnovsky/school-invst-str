fb77f01da1374cc249ead4fa225a6614c76a2ecc
Victoria Tran <vickictran@gmail.com>
2016-01-31 19:07:34 -0500

    Edits to report

--- Code/data_to_matlab_conv.R
+++ Code/data_to_matlab_conv.R
@@ -49,13 +49,13 @@ for(data in dir("Analyzed-Data/knn_chosen/")){
     Uo <- top10[[name]][school$Class+1,]
     go <- Uo$GR/Uo$phi
     suppressWarnings(
-      tmp1 <- mutate(tmp1, delta=1/(phi*Tui*(IP-OP)),g=go-GR/phi,
+      tmp1 <- mutate(tmp1, delta=1/(phi*(IP-OP)),g=go-GR/phi,
                      UNAME= paste(INSTNM,' (',UNITID,')'))
     )
     
     cls=rbind(cls,
      cbind(
-      tmp1[,c('UNAME','g','delta','phi')])
+      tmp1[,c('UNAME','g','delta','phi','Tui','R')])
     )
     
   }
@@ -65,13 +65,13 @@ for(data in dir("Analyzed-Data/knn_chosen/")){
     chosen <- mutate(chosen, ID = 1:length(UNAME))
     cls <- mutate(cls[!is.na(cls[,3]),], ID = 1:length(UNAME))
     
-    write.csv(apply(chosen[,2:5],2,as.numeric),
+    write.csv(apply(chosen[,2:7],2,as.numeric),
               "Matlab-Ready-Data/parted.data.csv",
               row.names = FALSE)
     write.csv(chosen[,c("ID","UNAME")],
               "Matlab-Ready-Data/parted.name.csv",
               row.names = FALSE)
-    write.csv(apply(cls[,2:5],2,as.numeric),
+    write.csv(apply(cls[,2:7],2,as.numeric),
               "Matlab-Ready-Data/full.data.csv",
               row.names = FALSE)
     write.csv(cls[,c("ID","UNAME")],
